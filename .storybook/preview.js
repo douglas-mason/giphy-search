b745f7e7c7b443b7829c7062af0cf71e5a58f1e9
Doug Mason <doug@subcurrent.com>
2021-02-04 01:42:57 -0500

    chore: add app styles to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import '../src/App.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <div style={{ margin: '3em' }}>
+      <Story />
+    </div>
+  ),
+];
